d895f4a8445ec7db59ce3e141432844931a07cf0
valenn0101 <veroncaceresvalentin@gmail.com>
2023-04-11 14:48:43 -0300

    Version 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const express = require('express');
+const v1Router = require('./v1/routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/v1', v1Router);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
