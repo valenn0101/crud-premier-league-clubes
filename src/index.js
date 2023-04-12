96b41c86b26316b5c7954aa4dbdbf3477b657a66
valenn0101 <veroncaceresvalentin@gmail.com>
2023-04-12 10:53:39 -0300

    Parsear todos los archivos JSON

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const v1ClubRouter = require('./v1/routes/clubRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use('/api/v1/club', v1ClubRouter);
 
 app.listen(PORT, () => {
