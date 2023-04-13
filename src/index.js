bbe5800505305ae6621002f4cfa1d383499dc41a
valenn0101 <veroncaceresvalentin@gmail.com>
2023-04-13 18:31:21 -0300

    Configuracion de Handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const v1ClubRouter = require('./v1/routes/clubRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const hbs = exphbs.create();
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/public', express.static('public'));
 app.use(express.json());
 app.use('/api/v1/clubs', v1ClubRouter);
 
